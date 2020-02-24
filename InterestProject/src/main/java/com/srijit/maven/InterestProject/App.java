f820fe849db720928081b1dfb6771ab3d9f1aa6d
SRIJIT PAUL <srijitpaulchoudhuri@gmail.com>
2020-02-24 22:47:28 +0530

    First Commit

--- InterestProject/src/main/java/com/srijit/maven/InterestProject/App.java
+++ InterestProject/src/main/java/com/srijit/maven/InterestProject/App.java
@@ -1,9 +1,12 @@
 package com.srijit.maven.InterestProject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.*;
 import java.io.*;
 
 class simple_interest
 {
+	private static final Logger LOGGER=LogManager.getLogger(App.class);  
    double p,r,t;
    simple_interest(double p,double r,double t)
    {
@@ -17,12 +20,14 @@ class simple_interest
        double result;
        result=p*(1+(r*t));
        
-         System.out.format("Final Amount = %.2f", result);
+         //System.out.format("Final Amount = %.2f", result);
+         LOGGER.info("Final Amount = " + result + "\n");
        
    }
 }
 class compound_interest
 {
+	private static final Logger LOGGER=LogManager.getLogger(App.class);
    double p,r,t,n;
    compound_interest(double p,double r,double t,double n)
    {
@@ -37,12 +42,15 @@ class compound_interest
        double result;
        result= p*(Math.pow((1+(r/n)),(n*t)));
       
-       System.out.format("Final Amount = %.2f", result);
+       //System.out.format("Final Amount = %.2f", result);
+       LOGGER.info("Final Amount = " + result + "\n");
+
        
    }
 }
 class house_cost
 {
+	private static final Logger LOGGER=LogManager.getLogger(App.class);
     double price,area;
     house_cost(double price,double area)
     {
@@ -51,12 +59,15 @@ class house_cost
     }
     void total_cost()
     {
-        System.out.format("Price of the entire construction = %.2f" , (price*area));
+        //System.out.format("Price of the entire construction = %.2f" , (price*area));
+        LOGGER.info("Price of the entire construction = " + (price*area));
     }
         
 }
 public class App 
 {
+	private static final Logger LOGGER=LogManager.getLogger(App.class);
+
     public static void main( String[] args )
     {
     	App.client_method();
@@ -66,49 +77,59 @@ public class App
         
         	
         Scanner scanner_object=new Scanner(System.in);
-         System.out.append("Press 1 for simple interest" + "\n");
-         System.out.append("Press 2 for compound interest");
+        // LOGGER.info("Press 1 for simple interest" + "\n");
+         //LOGGER.info("Press 2 for compound interest");
+         LOGGER.info("Press 1 for simple interest" + "\n");
+         LOGGER.info("Press 2 for compound interest" + "\n");
         int input= scanner_object.nextInt();
         switch(input)
         {
             case 1:
-                System.out.format("Enter Principal Amount");
+               // System.out.format("Enter Principal Amount");
+            	LOGGER.info("Enter Principal Amount");
                 double principal_amount=scanner_object.nextDouble();
-                 System.out.append("Enter annual rate of interest in decimal");
+                 //LOGGER.info("Enter annual rate of interest in decimal");
+                LOGGER.info("Enter annual rate of interest in decimal");
                 double rate_of_interest=scanner_object.nextDouble();
-                 System.out.append("Enter time period");
+                 //LOGGER.info("Enter time period");
+                LOGGER.info("Enter time period");
                 double time_period=scanner_object.nextDouble();
                 
                  simple_interest s=new simple_interest(principal_amount,rate_of_interest,time_period);
                  s.result();
                  break;
             case 2:
-                 System.out.append("Enter Principal Amount");
+                 //LOGGER.info("Enter Principal Amount");
+                 LOGGER.info("Enter Principal Amount");
                 double principal_amount_compound=scanner_object.nextDouble();
-                 System.out.append("Enter annual rate of interest in decimal");
+                // LOGGER.info("Enter annual rate of interest in decimal");
+                LOGGER.info("Enter annual rate of interest in decimal");
                 double rate_of_interest_compound=scanner_object.nextDouble();
-                 System.out.append("Enter time period");
+                // LOGGER.info("Enter time period");
+                LOGGER.info("Enter time period");
                 double time_period_compound=scanner_object.nextDouble();
-                 System.out.append("Enter number of compoundings");
+                 //LOGGER.info("Enter number of compoundings");
+                LOGGER.info("Enter number of compoundings");
                 double compounding=scanner_object.nextDouble();
                 
                  compound_interest c= new compound_interest(principal_amount_compound,rate_of_interest_compound,time_period_compound,compounding);
                  c.result();
                  break;
             default:
-                 System.out.append("Wrong Input");
+                 //LOGGER.info("Wrong Input");
+            	LOGGER.info("Wrong Input");
                 break;
                 
                 
         }
    
-        System.out.append("Press 1 if you want use standard building materials \n" );
-        System.out.append("Press 2 if you want use above standard building materials \n");
-        System.out.append("Press 3 if you want use high standard building materials without full automation \n");
-        System.out.append("Press 4 if you want use high standard building materials and full automation \n");
+        LOGGER.info("Press 1 if you want use standard building materials \n" );
+        LOGGER.info("Press 2 if you want use above standard building materials \n");
+        LOGGER.info("Press 3 if you want use high standard building materials without full automation \n");
+        LOGGER.info("Press 4 if you want use high standard building materials and full automation \n");
         
         input=scanner_object.nextInt();
-        System.out.append("Enter the area of the house");
+        LOGGER.info("Enter the area of the house");
         double area=scanner_object.nextInt();
         double cost;
         switch(input)
@@ -126,7 +147,7 @@ public class App
                 cost=2500;
                 break;
             default:
-                System.out.append("Wrong Input");
+                LOGGER.info("Wrong Input");
                 cost=0;
                 break;
                 
